174ed76fc08cebb07642e28d1f4c131b9df217f9
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-11-08 16:37:58 +0100

    chore

--- apps/main/src/routes/+layout.svelte
+++ apps/main/src/routes/+layout.svelte
@@ -18,17 +18,7 @@
 			<div class="flex items-center gap-2 px-4">
 				<Sidebar.Trigger class="-ml-1" />
 				<Separator orientation="vertical" class="mr-2 h-4" />
-				<Breadcrumb.Root>
-					<Breadcrumb.List>
-						<Breadcrumb.Item class="hidden md:block">
-							<Breadcrumb.Link href="#">Building Your Application</Breadcrumb.Link>
-						</Breadcrumb.Item>
-						<Breadcrumb.Separator class="hidden md:block" />
-						<Breadcrumb.Item>
-							<Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
-						</Breadcrumb.Item>
-					</Breadcrumb.List>
-				</Breadcrumb.Root>
+				<!-- TODO: Breadcrumb -->
 			</div>
 		</header>
 		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
